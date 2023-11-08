b33dd79a3ff7df71b29d697884daf750cc2fa0f1
Brenton-Lin <brenton.lin@yahoo.com>
2023-11-07 20:18:47 -0500

    27:06 - Screen init and first map
    -translated matrix of chars to on screen sprite positions
    -drew sprites at those positions

---
+++ PycharmProjects/onionKnight/level.py
@@ -0,0 +1,26 @@
+import pygame
+from settings import *
+from tile import *
+from player import Player
+class Level:
+    def __init__(self):
+        #get display
+        self.display_surface = pygame.display.get_surface()
+
+        # sprites setup
+        self.visible_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
+
+        self.create_map()
+
+    def create_map(self):
+        for y_index, row in enumerate(WORLD_MAP):
+            for x_index, x in enumerate(row):
+                x_screen = x_index * TILESIZE
+                y_screen = y_index * TILESIZE
+                if x == 'x':
+                    Tile((x_screen, y_screen),[self.visible_sprites])
+
+    def run(self):
+        self.visible_sprites.draw(self.display_surface)
+        pass
--- PycharmProjects/onionKnight/main.py
+++ PycharmProjects/onionKnight/main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
+import pygame, sys, level
+from settings import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Game:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption("Onion Knight")
+        self.level = level.Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            #the display class in the pygame namespace, has an update method
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    game = Game()
+    game.run()
